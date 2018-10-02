d241cb8e9c5a8704ecc2b8cfbbad95e3b9fca144
Kiel Dehnert <kielzor@gmail.com>
2018-10-02 17:22:24 -0600

    refactor random hex generator

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -19,14 +19,13 @@ function changePalettes() {
 function getRandomPalette(i) {
   if ($('.palette-' + i).hasClass('locked')) return
 
-  let color = []
+  let color = '#'
 
   for (let i = 0; i < 6; i++) {
-    let randomInt = Math.random() * (9 - 0) + 0
-    color.push(Math.round(randomInt))
+    let randomNum = Math.round(Math.random() * (9 - 0) + 0)
+    color = color + randomNum
   }
-  let newColor = color.join('')
   
-  if (i) $('.hex-' + i)[0].innerText = `#${newColor}`
-  return `#${newColor}`
+  if (i) $('.hex-' + i)[0].innerText = color
+  return color
 }
