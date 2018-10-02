c0d06d8a3001a2b92a39ca33ef8181a58a0abc44
Kiel Dehnert <kielzor@gmail.com>
2018-10-02 17:07:24 -0600

    add body coloring

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 
 app.set('port', process.env.PORT || 3000)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (request, response) => {
   response.send('Hello World!')
